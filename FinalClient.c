8d99d4922dfdc6957301b055aaf297a156579527
Znomon <kaseyfkolyno@gmail.com>
2015-11-13 12:45:28 -0500

    Initial Commit

---
+++ FinalClient.c
@@ -0,0 +1,158 @@
+//================================================= file = catdogClient.c =====
+//= A UDP client to send strings to catdogServer (with retry capability) =
+//=============================================================================
+//= Notes: =
+//= 1) This program conditionally compiles for Winsock and BSD sockets. =
+//= Set the initial #define to WIN or BSD as appropriate. =
+//= 2) This program assumes that the IP address of the host running =
+//= udpServer is defined in "#define IP_ADDR" =
+//= 3) The time-out is the #define as MAX_WAIT =
+//=---------------------------------------------------------------------------=
+//= Example execution: =
+//=---------------------------------------------------------------------------=
+//= Build: bcc32 catdogClient.c or cl catdogClient.c wsock32.lib for Winsock =
+//= gcc catdogClient.c -lsocket -lnsl for BSD =
+//=---------------------------------------------------------------------------=
+//= Execute: catdogClient =
+//=---------------------------------------------------------------------------=
+//= Author: Ken Christensen =
+//= University of South Florida =
+//= WWW: http://www.csee.usf.edu/~christen =
+//= Email: christen@csee.usf.edu =
+//=---------------------------------------------------------------------------=
+//= History: KJC (09/11/15) - Genesis (from udpClient.c) =
+//=============================================================================
+#define BSD // WIN for Winsock and BSD for BSD sockets
+//----- Include files ---------------------------------------------------------
+#include <stdio.h> // Needed for printf()
+#include <string.h> // Needed for memcpy() and strcpy()
+#include <stdlib.h> // Needed for exit()
+#include <time.h> // Needed for time()
+#ifdef WIN
+ #include <windows.h> // Needed for all Winsock stuff
+#endif
+#ifdef BSD
+ #include <sys/types.h> // Needed for sockets stuff
+ #include <netinet/in.h> // Needed for sockets stuff
+ #include <sys/socket.h> // Needed for sockets stuff
+ #include <arpa/inet.h> // Needed for sockets stuff
+ #include <fcntl.h> // Needed for sockets stuff
+ #include <netdb.h> // Needed for sockets stuff
+#endif
+//----- Defines ---------------------------------------------------------------
+#define PORT_NUM 1050 // Port number used
+#define IP_ADDR "66.158.200.153" // IP address of server1 (*** HARDWIRED ***)
+#define MAX_WAIT 5 // Maximum wait in seconds
+//===== Main program ==========================================================
+int main(int argc, char *argv[])
+{
+#ifdef WIN
+ WORD wVersionRequested = MAKEWORD(1,1); // Stuff for WSA functions
+ WSADATA wsaData; // Stuff for WSA functions
+#endif
+ int client_s; // Client socket descriptor
+ struct sockaddr_in server_addr; // Server Internet address
+  unsigned long int noBlock; // Non-blocking flag
+ int addr_len; // Internet address length
+ char out_buf[4096]; // Output buffer for data
+ char in_buf[4096]; // Input buffer for data
+ int retcode; // Return code
+ int timeStart; // Time of start for loop
+ int i; // Loop counter
+ 
+// Output usage
+/* if (argc != 2)
+ {
+ printf("Usage: catdogClient <string> where <string> is a maximum 140 \n");
+ printf(" character string to be cat/dog converted and reflected \n");
+ exit(1);
+ } */
+#ifdef WIN
+ // This stuff initializes winsock
+ WSAStartup(wVersionRequested, &wsaData);
+#endif
+ // Create a socket
+ client_s = socket(AF_INET, SOCK_DGRAM, 0);
+ if (client_s < 0)
+ {
+ printf("*** ERROR - socket() failed \n");
+ exit(-1);
+ }
+ #ifdef WIN
+ // Make client_s non-blocking
+ noBlock = 1;
+ ioctlsocket(client_s, FIONBIO, &noBlock);
+#endif
+#ifdef BSD
+ int flags = fcntl(client_s, F_GETFL, 0);
+ fcntl(client_s, F_SETFL, flags | O_NONBLOCK);
+#endif
+ // Fill-in server1 socket's address information
+ server_addr.sin_family = AF_INET; // Address family to use
+ server_addr.sin_port = htons(PORT_NUM); // Port num to use
+ server_addr.sin_addr.s_addr = inet_addr(IP_ADDR); // IP address to use
+ // Loop until receive reply message
+ timeStart = time(NULL);
+ while(1)
+ {
+   printf("Enter \"REG\" to register: ");
+   scanf("%s", out_buf);
+   printf("\n");
+   printf("Faggot \n");
+ // Assign a message to buffer out_buf
+ //strcpy(out_buf, argv[1]);
+ // Now send the message to server.
+ retcode = sendto(client_s, out_buf, (strlen(out_buf) + 1), 0,
+ (struct sockaddr *)&server_addr, sizeof(server_addr));
+ if (retcode < 0)
+ {
+ printf("*** ERROR - sendto() failed \n");
+ exit(-1);
+ } 
+  //printf("Send to server: %s \n", out_buf);
+ // Wait to receive a message
+ retcode = 0;
+ for (i=0; i<MAX_WAIT; i++)
+ {
+ addr_len = sizeof(server_addr);
+ retcode = recvfrom(client_s, in_buf, sizeof(in_buf), 0,
+ (struct sockaddr *)&server_addr, &addr_len);
+ if (retcode > 0)
+ {
+ printf("Elapsed time = %d seconds \n", time(NULL) - timeStart);
+ printf("Received from server: %s \n", in_buf);
+ 
+ }
+#ifdef WIN
+ Sleep(1000); // Windows Sleep for 1000 ms (1 second)
+#endif
+#ifdef BSD
+ sleep(1); // Unix sleep for 1 second
+#endif
+ }
+ }
+exit:
+ // Close all open sockets
+#ifdef WIN
+ retcode = closesocket(client_s);
+ if (retcode < 0)
+ {
+ printf("*** ERROR - closesocket() failed \n");
+ exit(-1);
+ }
+#endif
+#ifdef BSD
+ retcode = close(client_s);
+ if (retcode < 0)
+ {
+ printf("*** ERROR - close() failed \n");
+ exit(-1);
+ }
+#endif
+#ifdef WIN
+ // This stuff cleans-up winsock
+ WSACleanup();
+#endif
+ // Return zero and terminate
+ return(0);
+}
---
+++ FinalServer.c
@@ -0,0 +1,145 @@
+//================================================= file = catdogServer.c =====
+//= A UDP server to reflect strings (switching "cat" to "dog") with loss =
+//=============================================================================
+//= Notes: =
+//= 1) This program conditionally compiles for Winsock and BSD sockets. =
+//= Set the initial #define to WIN or BSD as appropriate. =
+//= 2) The message send loss probability is in the #define as LOSS_PROB =
+//=---------------------------------------------------------------------------=
+//= Example execution: =
+//= Waiting for recvfrom() to complete... =
+//= Received from client: "Test cat catalog excat 123!" =
+//= Sent to client: "Test dog dogalog exdog 123!" =
+//=---------------------------------------------------------------------------=
+//= Build: bcc32 catdogServer.c or cl catdogServer.c wsock32.lib for Winsock =
+//= gcc catdogServer.c -lsocket -lnsl for BSD =
+//=---------------------------------------------------------------------------=
+//= Execute: catdogServer =
+//=---------------------------------------------------------------------------=
+//= Author: Ken Christensen =
+//= University of South Florida =
+//= WWW: http://www.csee.usf.edu/~christen =
+//= Email: christen@csee.usf.edu =
+//=---------------------------------------------------------------------------=
+//= History: KJC (09/11/15) - Genesis (from udpServer.c) =
+//=============================================================================
+#define BSD // WIN for Winsock and BSD for BSD sockets
+//----- Include files --------------------------------------------------------
+#include <stdio.h> // Needed for printf()
+#include <string.h> // Needed for strcpy()
+#include <stdlib.h> // Needed for exit() and rand()
+#ifdef WIN
+#include <windows.h> // Needed for all Winsock stuff
+#endif
+#ifdef BSD
+#include <sys/types.h> // Needed for sockets stuff
+#include <netinet/in.h> // Needed for sockets stuff
+#include <sys/socket.h> // Needed for sockets stuff
+#include <arpa/inet.h> // Needed for sockets stuff
+#include <fcntl.h> // Needed for sockets stuff
+#include <netdb.h> // Needed for sockets stuff
+#endif
+//----- Defines --------------------------------------------------------------
+#define PORT_NUM 1050 // Arbitrary port number for the server
+int catdog(char *b); // Switch "cat" to "dog" in buffer b
+//===== Main program =========================================================
+int main() {
+  #ifdef WIN
+	WORD wVersionRequested = MAKEWORD(1, 1); // Stuff for WSA functions
+	WSADATA wsaData; // Stuff for WSA functions
+	#endif
+	int server_s; // Server socket descriptor
+	struct sockaddr_in server_addr; // Server Internet address
+	struct sockaddr_in client_addr; // Client Internet address
+	int addr_len; // Internet address length
+	char out_buf[4096]; // Output buffer for data
+	char in_buf[4096]; // Input buffer for data
+	int retcode; // Return code
+	#ifdef WIN
+	// This stuff initializes winsock
+	WSAStartup(wVersionRequested, & wsaData);
+  #endif
+	// Create a socket
+	server_s = socket(AF_INET, SOCK_DGRAM, 0);
+	if (server_s < 0) {
+		printf("*** ERROR - socket() failed \n");
+		exit(-1);
+	}
+	// Fill-in my socket's address information
+	server_addr.sin_family = AF_INET; // Address family to use
+	server_addr.sin_port = htons(PORT_NUM); // Port number to use
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY); // Listen on any IP address
+	retcode = bind(server_s, (struct sockaddr * ) & server_addr,
+	sizeof(server_addr));
+	if (retcode < 0) {
+		printf("*** ERROR - bind() failed \n");
+		exit(-1);
+	}
+	//Loop forever
+	while (1) {
+		// Wait to receive a message from client
+		printf("Waiting for recvfrom() to complete... \n");
+		addr_len = sizeof(client_addr);
+		retcode = recvfrom(server_s, in_buf, sizeof(in_buf), 0, (struct sockaddr * ) & client_addr, & addr_len);
+		if (retcode < 0) {
+			printf("*** ERROR - recvfrom() failed \n");
+			exit(-1);
+		}
+		printf(" Received from client: %s \n", in_buf);
+		// Switch "cat" to "dog" in in_buf and copy in_buf to out_buf
+    
+    char buffer[400];
+    char *test = buffer;
+    
+    if(catdog(in_buf) == 3)
+    {
+      sprintf(test, "Your username is %s", in_buf);
+      strcpy(out_buf, buffer);
+    }
+    
+    
+		//strcpy(out_buf, in_buf);
+
+		// Send out_buf to the client using the server socket
+		retcode = sendto(server_s, out_buf, (strlen(out_buf) + 1), 0, (struct sockaddr * ) & client_addr, sizeof(client_addr));
+		if (retcode < 0) {
+			printf("*** ERROR - sendto() failed \n");
+			exit(-1);
+		}
+		printf(" Sent to client: %s \n", out_buf);
+	}
+	// Close all open sockets
+	#ifdef WIN
+	retcode = closesocket(server_s);
+	if (retcode < 0) {
+		printf("*** ERROR - closesocket() failed \n");
+		exit(-1);
+	}
+  #endif
+  #ifdef BSD
+	retcode = close(server_s);
+	if (retcode < 0) {
+		printf("*** ERROR - close() failed \n");
+		exit(-1);
+	}
+  #endif
+  #ifdef WIN
+	// This stuff cleans-up winsock
+	WSACleanup();
+  #endif
+	// Return zero and terminate
+	return (0);
+}
+
+int catdog(char * b) {
+	int n; // Length of string b
+	int i; // Loop index
+	// Get length of string b
+	n = strlen(b);
+	// Replace "cat" with "dog" all instances in string b
+	for (i = 0; i <= n; i++) {
+		if ((b[i] == 'R') && (b[i + 1] == 'E') && (b[i + 2] == 'G')) {
+      return 3;
+		}
+	}
+}
