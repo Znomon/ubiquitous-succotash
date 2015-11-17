4c7a6b364dcb7fc8bc66f51bbb4c9cb2ca595cbd
Znomon <kaseyfkolyno@gmail.com>
2015-11-17 15:24:09 -0500

    --hi ryan

--- FinalClient.c
+++ FinalClient.c
@@ -141,7 +141,6 @@ int main(int argc, char * argv[]) {
 					else {
 						printf("Usename not on file, please register with (1) or login with (3).\n\n");
 						break;
-						//hi ryan
 					}
 				case 5:
 					goto exit;
